299e45cd2427aebb7bde560208287f4a8c924db5
Andrew Nielson <29068131+drewnielson@users.noreply.github.com>
2018-11-18 22:16:17 -0500

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -31,7 +31,7 @@ $(function() {
     var $set = $(this)
       .nextUntil(".tag-h1")
       .add();
-    $set.wrapAll('<div class="testSection" />');
+    $set.wrapAll('<ul class="testSection" />');
   });
 
   sectionHeight();
