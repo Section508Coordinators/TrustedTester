0fbd91d415678fa542c061295f141ac79dbd7d83
Andrew Nielson <29068131+drewnielson@users.noreply.github.com>
2018-11-18 21:39:08 -0500

    Delete mainmodify.js

--- docs/assets/js/mainmodify.js
+++
@@ -1,6 +0,0 @@
-$(".tag-h1:contains(Section 508 Conformance Tests)").each(function(){
-    var $set = $(this)
-      .nextUntil(".tag-h1")
-      .add();
-    $set.wrapAll('<div class="testSection" />');
-  });
